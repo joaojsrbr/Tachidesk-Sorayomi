7bfa72ebfb63cbf50e97d6f2045311dd1a098037
João Vitor <joaovitor.jsr@gmail.com>
2022-05-08 03:20:16 -0300

    new

--- lib/app/core/values/string.dart
+++ lib/app/core/values/string.dart
@@ -1,7 +1,15 @@
+// const String sorayomiGithubUrl =
+//     "https://github.com/Suwayomi/Tachidesk-Sorayomi";
+
+// const String sorayomiLatestReleaseUrl =
+//     "https://github.com/Suwayomi/Tachidesk-Sorayomi/releases/latest";
+// const String sorayomiLatestReleaseApiUrl =
+//     "https://api.github.com/repos/Suwayomi/Tachidesk-Sorayomi/releases/latest";
+
 const String sorayomiGithubUrl =
-    "https://github.com/Suwayomi/Tachidesk-Sorayomi";
+    "https://github.com/joaojsrbr/Tachidesk-Sorayomi";
 
 const String sorayomiLatestReleaseUrl =
-    "https://github.com/Suwayomi/Tachidesk-Sorayomi/releases/latest";
+    "https://github.com/joaojsrbr/Tachidesk-Sorayomi/releases/latest";
 const String sorayomiLatestReleaseApiUrl =
-    "https://api.github.com/repos/Suwayomi/Tachidesk-Sorayomi/releases/latest";
+    "https://api.github.com/repos/joaojsrbr/Tachidesk-Sorayomi/releases/latest";
